cc771aed38350b763e825dadbe6ca2a82600cf0e
Siyuan-Harry <siyuanfang@foxmail.com>
2023-11-28 19:37:53 +0800

    上传faiss index和csv

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 import numpy as np
+import openai
+import streamlit as st
 
 
 def searchVDB(search_sentence, paraphrase_embeddings_df, index):
@@ -26,3 +28,41 @@ def searchVDB(search_sentence, paraphrase_embeddings_df, index):
         retrieved_chunks_list = []
         
     return retrieved_chunks_list
+
+
+st.title("💡AI助教")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+prompt = st.chat_input("What is up?")
+
+if prompt:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
