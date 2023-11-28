c58cd32c2dc40663f7b443596a4befc854947416
Siyuan-Harry <siyuanfang@foxmail.com>
2023-11-28 23:51:57 +0800

    取消报错机制

--- app.py
+++ app.py
@@ -13,25 +13,25 @@ def get_vdb():
 
 def searchVDB(search_sentence, paraphrase_embeddings_df, index):
     #从向量数据库中检索相应文段
-    try:
-        data = paraphrase_embeddings_df
-        embeddings = data.iloc[:, 1:].values  # All columns except the first (chunk text)
-        embeddings = np.ascontiguousarray(embeddings, dtype=np.float32)
+    #try:
+    data = paraphrase_embeddings_df
+    embeddings = data.iloc[:, 1:].values  # All columns except the first (chunk text)
+    embeddings = np.ascontiguousarray(embeddings, dtype=np.float32)
 
-        model = SentenceTransformer('paraphrase-mpnet-base-v2')
-        sentence_embedding = model.encode([search_sentence])
+    model = SentenceTransformer('paraphrase-mpnet-base-v2')
+    sentence_embedding = model.encode([search_sentence])
 
-        # Ensuring the sentence embedding is in the correct format
-        sentence_embedding = np.ascontiguousarray(sentence_embedding, dtype=np.float32)
-        # Searching for the top 3 nearest neighbors in the FAISS index
-        D, I = index.search(sentence_embedding, k=3)
-        # Printing the top 3 most similar text chunks
-        retrieved_chunks_list = []
-        for idx in I[0]:
-            retrieved_chunks_list.append(data.iloc[idx].chunk)
+    # Ensuring the sentence embedding is in the correct format
+    sentence_embedding = np.ascontiguousarray(sentence_embedding, dtype=np.float32)
+    # Searching for the top 3 nearest neighbors in the FAISS index
+    D, I = index.search(sentence_embedding, k=3)
+    # Printing the top 3 most similar text chunks
+    retrieved_chunks_list = []
+    for idx in I[0]:
+        retrieved_chunks_list.append(data.iloc[idx].chunk)
 
-    except Exception:
-        retrieved_chunks_list = []
+    #except Exception:
+        #retrieved_chunks_list = []
         
     return retrieved_chunks_list
 
@@ -76,7 +76,6 @@ def app():
             st.markdown(user_question)
         
         embeddings_df, faiss_index = get_vdb()
-        st.write(embeddings_df)
         retrieved_chunks_for_user = searchVDB(user_question, embeddings_df, faiss_index)
         st.write(retrieved_chunks_for_user)
         prompt = decorate_user_question(user_question, retrieved_chunks_for_user)
