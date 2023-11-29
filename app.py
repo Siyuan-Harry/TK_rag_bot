94b6ba8669e2a015b74347d29364ac8f2e536ea2
Siyuan-Harry <siyuanfang@foxmail.com>
2023-11-29 13:18:35 +0800

    检查嵌入的效果

--- app.py
+++ app.py
@@ -13,25 +13,22 @@ def get_vdb():
 
 def searchVDB(search_sentence, paraphrase_embeddings_df, index):
     #从向量数据库中检索相应文段
-    #try:
     data = paraphrase_embeddings_df
     embeddings = data.iloc[:, 2:].values  # All columns except the first (chunk text)
     embeddings = np.ascontiguousarray(embeddings, dtype=np.float32)
 
     model = SentenceTransformer('paraphrase-multilingual-mpnet-base-v2')
     sentence_embedding = model.encode([search_sentence])
+    st.write(search_sentence)
+    st.write(sentence_embedding)
 
     # Ensuring the sentence embedding is in the correct format
     sentence_embedding = np.ascontiguousarray(sentence_embedding, dtype=np.float32)
     # Searching for the top 3 nearest neighbors in the FAISS index
     D, I = index.search(sentence_embedding, k=5)
-    # Printing the top 3 most similar text chunks
     retrieved_chunks_list = []
     for idx in I[0]:
         retrieved_chunks_list.append(data.iloc[idx].chunk)
-
-    #except Exception:
-        #retrieved_chunks_list = []
         
     return retrieved_chunks_list
 
@@ -45,7 +42,6 @@ def decorate_user_question(user_question, retrieved_chunks_for_user):
     Now I'm the student, please answer it for me.
     '''
     return decorated_prompt
-
 
 
 def app():
